4389d2da34dd5a26965d383a4aa16a8d3dc0af44
parkjain <elly000417@gmail.com>
2018-06-25 14:12:40 +0900

    커밋커밋!

--- psps/UploadImages.swift
+++ psps/UploadImages.swift
@@ -19,6 +19,36 @@ var arr6 : Array<UIImage> = []
 var arrsection : Array<String> = []
 var arrCombined : NSMutableArray!
 
+
+extension UIImage {
+    func buffer(with size:CGSize) -> CVPixelBuffer? {
+        if let image = self.cgImage {
+            let frameSize = size
+            var pixelBuffer:CVPixelBuffer? = nil
+            let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+            if status != kCVReturnSuccess {
+                return nil
+            }
+            CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+            let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+            let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+            let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+            context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+            CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+            
+            return pixelBuffer
+        }else{
+            return nil
+        }
+    }
+}
+
+
+
+
+
+
 @available(iOS 11.0, *)
 class UploadImages: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -58,42 +88,32 @@ class UploadImages: UIViewController, UICollectionViewDelegate, UICollectionView
     }
     
     @IBAction func uploadbuttonTapped(_ sender: Any) {
-        
-        // 분류 결과 받아서 변수에 저장
-        
-       for i in 0 ..< Images.count {
+        for i in 0 ..< Images.count {
         
             let model = ssd_mobilenet()
-        
-            guard let ssd_mobilenetOutput = try? model.prediction(Preprocessor__sub__0: Images[i] as! CVPixelBuffer) else {
-                fatalError("Unexpected runtime error.")
+            var result = NSNumber()
+            if let buffer = Images[i].buffer(with: CGSize(width:300, height:300)) {
+                guard let output = try? model.prediction(Preprocessor__sub__0: buffer) else {fatalError("Unexpected runtime error")}
+                result = output.concat_1__0[0]
+                print("결과 : ",result)
+
+            }else{
+                print("failed buffer")
             }
         
-        
-      //let result = ssd_mobilenetOutput.concat_1__0
-        let result = i%6
-        
-        print("결과 : ",result)
-        
         //잘 돌아가는지 한 번 보자!
-        
-        switch result{
-        case 0 : arr1.append(Images[i])
-        case 1 : arr2.append(Images[i])
-        case 2 : arr3.append(Images[i])
-        case 3 : arr4.append(Images[i])
-        case 4 : arr5.append(Images[i])
-        case 5 : arr6.append(Images[i])
-            
-        default:
-            arr1.append(Images[i])
+          let result2 = i%6
+            switch result2{
+                case 0 : arr1.append(Images[i])
+                case 1 : arr2.append(Images[i])
+                case 2 : arr3.append(Images[i])
+                case 3 : arr4.append(Images[i])
+                case 4 : arr5.append(Images[i])
+                case 5 : arr6.append(Images[i])
+                default: arr1.append(Images[i])
+            }
         }
-        
-            
-        }
-        
         performSegue(withIdentifier: "gotoResult", sender: self)
-        
    }
     
     @IBAction func gotoAdd(_ sender: Any) {
